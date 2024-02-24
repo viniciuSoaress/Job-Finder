76fe4a312ee8488485ad552e85ac89615acfb831
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-02-24 14:54:16 -0300

    create Hero

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,19 @@
+
+export function Hero() {
+  return (
+    <div className="w-full h-[80vh] flex items-center md:justify-between justify-center md:px-24 px-5 bg-cover bg-no-repeat bg-center relative"
+      style={{ backgroundImage: `url(./images/bg.jpg)` }}
+    >
+      <div className="flex flex-col gap-8 z-50 items-start">
+        <span className='text-5xl font-semibold text-indigo-100'>
+          Welcome tTo Job Finder
+        </span>
+        <p className="text-gray-200 max-w-[660px] leading-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores esse pariatur minus, sit nostrum accusantium, cum impedit quod, facere eos sed sunt incidunt vitae tempora nihil ex! Officia, ut culpa?</p>
+        <button className="text-white rounded-full bg-indigo-500 w-40 p-3">
+          Find Jobs Now
+        </button>
+        <div className="w-full h-[calc(80vh-60px)] z-10 absolute top-[56px] bg-indigo-900/30 left-0 ring-0" />
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import { Hero } from "./components/Hero";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <h1>Job Finder</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+
+      <Hero />
     </main>
   );
 }
